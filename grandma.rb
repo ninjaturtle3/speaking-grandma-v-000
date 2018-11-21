e27f4df65c73508910c29e5333bf7eaaea732a84
Lluvia Frias <lluvi.frias@gmail.com>
2018-11-21 02:27:48 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,12 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
+    puts "I LOVE YOU TOO PUMPKIN!"
 
   elsif phrase == phrase.upcase
-    "NO, NOT SINCE 1938!"
+    puts "NO, NOT SINCE 1938!"
 
   else phrase == phrase.downcase
-    "HUH?! SPEAK UP, SONNY!"
+    puts "HUH?! SPEAK UP, SONNY!"
 
 end
 end
